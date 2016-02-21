82943ce69cb1db798bc2d45cee37f2665ab9735f
Ka <k@ka.st>
2016-02-21 13:44:07 +0100

    Fixes for Travis-CI build and default Domion config

--- domion-setup.sh
+++ domion-setup.sh
@@ -2,6 +2,10 @@
 
 scripts/feeds update -a
 scripts/feeds install domoticz
+
+#Horrible but quicker than forking the repo
+sed -i '' 's/+libxml2/+libxml2 +libubacktrace/g' feeds/packages/libs/db47/Makefile 
+
 cp domion.config .config
 make defconfig
 if [ "$FULL_VERBOSE_LOG" != "1" ]; then
